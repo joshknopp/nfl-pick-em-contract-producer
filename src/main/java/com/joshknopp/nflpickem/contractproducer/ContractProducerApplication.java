4286d41cfb1057a93d7272b209c8cac5fabab12d
joshknopp <github@joshknopp.com>
2020-10-09 21:40:37 -0400

    Following tutorial

---
+++ src/main/java/com/joshknopp/nflpickem/contractproducer/ContractProducerApplication.java
@@ -0,0 +1,13 @@
+package com.joshknopp.nflpickem.contractproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joshknopp/nflpickem/contractproducer/ContractProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshknopp.nflpickem.contractproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
